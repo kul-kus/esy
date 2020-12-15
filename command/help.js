f02ae0c1671eb5c3254c4cf0aa8a46b3ee7adbc2
KULK <kulk@eur.ad.sag@SAG-7CKFMQ2-U.EUR.AD.SAG>
2020-12-15 10:58:44 +0530

    help updated

--- command/help.js
+++ command/help.js
@@ -9,16 +9,6 @@ colors.setTheme({
 
 module.exports = {
     help: function () {
-
-        let output = ""
-        output += ' \n';
-        output += '          ░█████╗░██╗░░░░░███████╗░██╗░░██╗░█████╗░\n';
-        output += '          ██╔══██╗██║░░░░░██╔════╝░╚██╗██╔╝██╔══██╗\n';
-        output += '          ███████║██║░░░░░█████╗░░░░╚███╔╝░███████║\n';
-        output += '          ██╔══██║██║░░░░░██╔══╝░░░░██╔██╗░██╔══██║\n';
-        output += '          ██║░░██║███████╗███████╗░██╔╝╚██╗██║░░██║\n';
-        output += '          ╚═╝░░╚═╝╚══════╝╚══════╝░╚═╝░░╚═╝╚═╝░░╚═╝\n';
-
         let esy = ""
         esy += ' \n';
         esy += '          █████████╗░████████╗███╗░░░░███╗\n';
@@ -30,57 +20,33 @@ module.exports = {
 
 
         table = new Table({ head: ["Title", "Command", "Info"] });
-        // table = new Table({ style: { head: [], border: [] } })
 
         table.push(
-            // [{ colSpan: 3, content: output }],
-            // ["Title".heading, "Command".heading, "Info".heading],
+
+            // ---git hub commands------
+            ["Push code on github", "esy push <commit message> [--copy | -c ]", "Push code on current branch."],
+            ["Pull current branch", "esy pull", "Pull code on current branch."],
+            ["Checkout Specific github Branch", "esy checkout <branch name> [--all]", "Checkout to a specific branch."],
+            ["current branch", "esy branch", "Prints current branch"],
+            ["Store Git Credential", "esy store", "Store Git credentials, (git config credential.helper store)"],
 
 
             //----- wmio commands---
-            ["Who", "esy whoami", "Shows config file content"],
-            // ["Open", "esy open", "Opens the .connector folder in Visual studio Code."],
             ["Change Config", "esy change <filter param> [--deploy]", "Provides a list of file to set config.json and the connector to specified tenant."],
             ["Show Config", "esy show <filter param>", "Provides a list of file to show File Content."],
             ["Create File", "esy create <file name> [--code | --nano]", "Create a new file with user specified name."],
             ["Update File", "esy update <filter param> [--code | --nano]", "Updates the content of specified file."],
             ["Rename File", "esy rename <filter param> [-- new file name]", "Rename specific file."],
             ["Delete File", "esy delete  <filter param>", "Deletes the selected file."],
-            // ["Migrate", "esy migrate", "Migrates the conncetor to specified tenant."],
-
-            ["Create Schema", "esy schema", "Create schema for specified json object."],
-            ["Create common file", "esy create_common", "creates common file with user defined functions to develop Connectors."],
-
-            // ---git hub commands------
-            ["Checkout Specific github Branch", "esy checkout <branch name> [--all]", "Checkout to a specific branch."],
-            ["Push code on github", "esy push <commit message> [--copy | -c ]", "Push code on current branch."],
-            ["Pull current branch", "esy pull", "Pull code on current branch."],
-            ["current branch", "esy branch", "Prints current branch"],
-
-
-            //------------ other commands------
-            ["Shutdown Laptop", "esy shutdown | esy off | esy poweroff", "Shutdown the Laptop"],
-            ["Store Git Credential", "esy store", "Store Git credentials, (git config credential.helper store)"],
-            ["Night Node", "esy nightmode < on | off >", "Enable Night Node"],
+            ["Who", "esy whoami", "Shows config file content"],
             ["Kill Process", "esy kill <Port number> [-a]", "Kills process of a specific port"],
-
-
-            //--------------- to open files-----
-            ["Opens specified Repo", "esy open [git | config] <file name>", "Provides list of Repository and opens them in Visual studio Code."],
-
-
-
-            //----------- help commads----
-            ["Help", "esy help or esy", "List all commands"],
-            ["Restart Laptop", "esy restart | esy reboot", "Restarts the Laptop"],
-            ["Version", "esy --version | esy -v", "List version of your esy bot"]
+            ["Create Schema", "esy schema", "Create schema for specified json object."],
+            ["Help", "esy help or esy", "List all commands you can use"],
+            ["Version", "esy --version | esy -v", "List the current version of your esy npm module"]
 
         );
         console.log()
-
-
         console.log(esy);
-
         console.log(table.toString());
         console.log()
     }
